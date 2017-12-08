72e99abd27cecfe748f3a4591ecb0c95ee5a5d8d
Daniël van Drunen <daniel@pixely.me>
2017-12-08 22:40:12 +0100

    Add news command

---
+++ dictionary/news.js
@@ -0,0 +1,19 @@
+const request = require('request')
+const xml2js = require('xml2js')
+const moment = require('moment')
+
+const parser = new xml2js.Parser()
+
+module.exports = (config, arguments, respond) => {
+    request('https://www.nu.nl/rss/Algemeen', (error, response, data) => {
+        parser.parseString(data, (error, result) => {
+            let articles = ''
+
+            result.rss.channel[0].item.forEach(item => {
+                articles += item.title + ' · ' + moment(item.pubDate, 'ddd, DD MMM YYYY HH:mm:ss').format('HH:mm') + ' · <' + item.link + '>\n\n'
+            })
+
+            respond([articles])
+        })
+    })
+};
